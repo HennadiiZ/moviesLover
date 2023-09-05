28196eb80d6c37c0b0ae02327b1f280405399792
Hennadii Zhukov <hennadii.zhukov@jetsoftpro@com>
2023-09-05 15:17:52 -0600

    Component Composition for NavBar

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ import tempWatchedData from './data/tempMovieData';
 import NavBar from './components/NavBar';
 import Main from './components/Main';
 
+import Logo from './components/Logo';
+import Search from './components/Search';
+import NumberResults from './components/NumberResults';
+
 const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
@@ -13,7 +17,12 @@ export default function App() {
 
   return (
     <>
-      <NavBar movies={movies} />
+      {/* <NavBar movies={movies} /> */}
+      <NavBar>
+        <Logo />
+        <Search />
+        <NumberResults movies={movies} />
+      </NavBar>
       <Main
         movies={movies}
         tempWatchedData={tempWatchedData}
@@ -22,133 +31,3 @@ export default function App() {
     </>
   );
 }
-//---------------------------------------------------
-// import { useState } from 'react';
-// import tempMovieData from './data/tempMovieData';
-// import tempWatchedData from './data/tempMovieData';
-
-// import NavBar from './components/NavBar';
-
-// const average = (arr) =>
-//   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
-
-// export default function App() {
-//   const [movies, setMovies] = useState(tempMovieData);
-
-//   return (
-//     <>
-//       <NavBar movies={movies} />
-//       <Main
-//         movies={movies}
-//         tempWatchedData={tempWatchedData}
-//         average={average}
-//       />
-//     </>
-//   );
-// }
-
-// function NavBar({ movies }) {
-//   return (
-//     <nav className='nav-bar'>
-//       <Logo />
-//       <Search />
-//       <NumberResults movies={movies} />
-//     </nav>
-//   );
-// }
-
-// function Main({ movies, tempWatchedData, average }) {
-//   const [watched, setWatched] = useState(tempWatchedData);
-//   const [isOpen1, setIsOpen1] = useState(true);
-//   const [isOpen2, setIsOpen2] = useState(true);
-
-//   const avgImdbRating = average(watched.map((movie) => movie.imdbRating));
-//   const avgUserRating = average(watched.map((movie) => movie.userRating));
-//   const avgRuntime = average(watched.map((movie) => movie.runtime));
-
-//   return (
-//     <main className='main'>
-//       <div className='box'>
-//         <button
-//           className='btn-toggle'
-//           onClick={() => setIsOpen1((open) => !open)}
-//         >
-//           {isOpen1 ? '–' : '+'}
-//         </button>
-//         {isOpen1 && (
-//           <ul className='list'>
-//             {movies?.map((movie) => (
-//               <li key={movie.imdbID}>
-//                 <img src={movie.Poster} alt={`${movie.Title} poster`} />
-//                 <h3>{movie.Title}</h3>
-//                 <div>
-//                   <p>
-//                     <span>🗓</span>
-//                     <span>{movie.Year}</span>
-//                   </p>
-//                 </div>
-//               </li>
-//             ))}
-//           </ul>
-//         )}
-//       </div>
-
-//       <div className='box'>
-//         <button
-//           className='btn-toggle'
-//           onClick={() => setIsOpen2((open) => !open)}
-//         >
-//           {isOpen2 ? '–' : '+'}
-//         </button>
-//         {isOpen2 && (
-//           <>
-//             <div className='summary'>
-//               <h2>Movies you watched</h2>
-//               <div>
-//                 <p>
-//                   <span>#️⃣</span>
-//                   <span>{watched.length} movies</span>
-//                 </p>
-//                 <p>
-//                   <span>⭐️</span>
-//                   <span>{avgImdbRating}</span>
-//                 </p>
-//                 <p>
-//                   <span>🌟</span>
-//                   <span>{avgUserRating}</span>
-//                 </p>
-//                 <p>
-//                   <span>⏳</span>
-//                   <span>{avgRuntime} min</span>
-//                 </p>
-//               </div>
-//             </div>
-
-//             <ul className='list'>
-//               {watched.map((movie) => (
-//                 <li key={movie.imdbID}>
-//                   <img src={movie.Poster} alt={`${movie.Title} poster`} />
-//                   <h3>{movie.Title}</h3>
-//                   <div>
-//                     <p>
-//                       <span>⭐️</span>
-//                       <span>{movie.imdbRating}</span>
-//                     </p>
-//                     <p>
-//                       <span>🌟</span>
-//                       <span>{movie.userRating}</span>
-//                     </p>
-//                     <p>
-//                       <span>⏳</span>
-//                       <span>{movie.runtime} min</span>
-//                     </p>
-//                   </div>
-//                 </li>
-//               ))}
-//             </ul>
-//           </>
-//         )}
-//       </div>
-//     </main>
-//   );
-// }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,16 @@
 import Search from './Search';
-import Logo from './Search';
+import Logo from './Logo';
 import NumberResults from './NumberResults';
 
-export default function NavBar({ movies }) {
+export default function NavBar({ children }) {
+  console.log(children[2].props);
   return (
-    <nav className='nav-bar'>
-      {/* <Logo /> */}
-      <Search />
-      <NumberResults movies={movies} />
-    </nav>
+    // <nav className='nav-bar'>
+    //   <Logo />
+    //   <Search />
+    //   <NumberResults movies={movies} />
+    // </nav>
+
+    <nav className='nav-bar'>{children}</nav>
   );
 }
