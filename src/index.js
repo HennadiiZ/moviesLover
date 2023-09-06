60d92b0e7a50c84de50a2c019a986ccc02a286db
Hennadii Zhukov <hennadii.zhukov@jetsoftpro@com>
2023-09-06 15:05:29 -0600

    reusable starRating is done

--- src/index.js
+++ src/index.js
@@ -9,7 +9,18 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5} />
+    {/* <StarRating maxRating={5} color={'#fcc419'} size={48} /> */}
+    <StarRating
+      maxRating={5}
+      messages={['Terrible', 'Bad', 'Okay', 'Good', 'Amazing']}
+    />
+    <StarRating
+      size={24}
+      color='red'
+      className='test'
+      defaultRating={3}
+      onSetRating={{}}
+    />
     {/* <StarRating maxRating={7} /> */}
   </React.StrictMode>
 );
