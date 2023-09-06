15732f43d20829641cb0d59cff55f0a14f68a84b
Hennadii Zhukov <hennadii.zhukov@jetsoftpro@com>
2023-09-06 15:49:47 -0600

    small update

--- src/index.js
+++ src/index.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import './index.css';
-// import App from './App';
+import './index.css';
+import App from './App';
 
 import StarRating from './components/StarRating';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    {/* <StarRating maxRating={5} color={'#fcc419'} size={48} /> */}
+    <App />
+
+    <StarRating maxRating={5} color={'#fcc419'} size={48} />
+
     <StarRating
       maxRating={5}
       messages={['Terrible', 'Bad', 'Okay', 'Good', 'Amazing']}
     />
+
     <StarRating
       size={24}
       color='red'
@@ -21,6 +24,5 @@ root.render(
       defaultRating={3}
       onSetRating={{}}
     />
-    {/* <StarRating maxRating={7} /> */}
   </React.StrictMode>
 );
