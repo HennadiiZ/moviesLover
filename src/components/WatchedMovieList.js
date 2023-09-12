9de7ff23bff57038432dcbfc04b6b75efdfa85e9
Hennadii Zhukov <hennadii.zhukov@jetsoftpro@com>
2023-09-11 20:15:08 -0600

    fully done

--- src/components/WatchedMovieList.js
+++ src/components/WatchedMovieList.js
@@ -1,14 +1,28 @@
-import Movie from './Movie';
+// import Movie from './Movie';
 import WatchedMovie from './WatchedMovie';
-import MovieRate from './MovieRate';
+// import MovieRate from './MovieRate';
 
-export default function WatchedMovieList({ movies }) {
+// export default function WatchedMovieList({ movies }) {
+//   return (
+//     <ul className='list'>
+//       {movies.map((movie) => (
+//         <WatchedMovie movie={movie} key={movie.imdbID}>
+//           <MovieRate movie={movie} />
+//         </WatchedMovie>
+//       ))}
+//     </ul>
+//   );
+// }
+
+export default function WatchedMoviesList({ watched, onDeleteWatched }) {
   return (
     <ul className='list'>
-      {movies.map((movie) => (
-        <WatchedMovie movie={movie} key={movie.imdbID}>
-          <MovieRate movie={movie} />
-        </WatchedMovie>
+      {watched.map((movie) => (
+        <WatchedMovie
+          movie={movie}
+          key={movie.imdbID}
+          onDeleteWatched={onDeleteWatched}
+        />
       ))}
     </ul>
   );
