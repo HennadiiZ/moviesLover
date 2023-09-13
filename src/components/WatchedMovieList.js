e514a87dcafc6604b5247ad6a062627850a83842
Hennadii Zhukov <hennadii.zhukov@jetsoftpro@com>
2023-09-13 14:43:43 -0600

    preparing my code base

--- src/components/WatchedMovieList.js
+++ src/components/WatchedMovieList.js
@@ -13,19 +13,3 @@ export default function WatchedMoviesList({ watched, onDeleteWatched }) {
     </ul>
   );
 }
-
-// import WatchedMovie from './WatchedMovie';
-
-// export default function WatchedMoviesList({ watched, onDeleteWatched }) {
-//   return (
-//     <ul className='list'>
-//       {watched.map((movie) => (
-//         <WatchedMovie
-//           movie={movie}
-//           key={movie.imdbID}
-//           onDeleteWatched={onDeleteWatched}
-//         />
-//       ))}
-//     </ul>
-//   );
-// }
