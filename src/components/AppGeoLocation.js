73961b101878822260e4a3959f41b312cb93a0ab
Hennadii Zhukov <hennadii.zhukov@jetsoftpro@com>
2023-09-15 13:40:35 -0600

    creating useGeolocation custom hook

--- src/components/AppGeoLocation.js
+++ src/components/AppGeoLocation.js
@@ -2,34 +2,38 @@ import { useState } from 'react';
 import { useGeolocation } from '../customHooks/useGeolocation';
 
 export default function AppGeoLocation() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [countClicks, setCountClicks] = useState(0);
-  const [position, setPosition] = useState({});
-  const [error, setError] = useState(null);
+  //   const [isLoading, setIsLoading] = useState(false);
+  //   const [countClicks, setCountClicks] = useState(0);
+  //   const [position, setPosition] = useState({});
+  //   const [error, setError] = useState(null);
 
+  //   const { lat, lng } = position;
+
+  //   function getPosition() {
+  //     setCountClicks((count) => count + 1);
+
+  //     if (!navigator.geolocation)
+  //       return setError('Your browser does not support geolocation');
+
+  //     setIsLoading(true);
+  //     navigator.geolocation.getCurrentPosition(
+  //       (pos) => {
+  //         setPosition({
+  //           lat: pos.coords.latitude,
+  //           lng: pos.coords.longitude,
+  //         });
+  //         setIsLoading(false);
+  //       },
+  //       (error) => {
+  //         setError(error.message);
+  //         setIsLoading(false);
+  //       }
+  //     );
+  //   }
+
+  const [isLoading, countClicks, position, error, getPosition] =
+    useGeolocation();
   const { lat, lng } = position;
-
-  function getPosition() {
-    setCountClicks((count) => count + 1);
-
-    if (!navigator.geolocation)
-      return setError('Your browser does not support geolocation');
-
-    setIsLoading(true);
-    navigator.geolocation.getCurrentPosition(
-      (pos) => {
-        setPosition({
-          lat: pos.coords.latitude,
-          lng: pos.coords.longitude,
-        });
-        setIsLoading(false);
-      },
-      (error) => {
-        setError(error.message);
-        setIsLoading(false);
-      }
-    );
-  }
 
   return (
     <div>
