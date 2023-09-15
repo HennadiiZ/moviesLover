eaf9477cd858e9823c38863498df40833ee992fe
Hennadii Zhukov <hennadii.zhukov@jetsoftpro@com>
2023-09-15 13:00:57 -0600

    deleting comments

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,9 +1,8 @@
-import { useEffect, useRef } from 'react';
+import { useRef } from 'react';
 import { useKey } from '../customHooks/useKey.js';
 
 export default function Search({ query, setQuery }) {
   const inputEl = useRef(null);
-  // console.log(inputEl);
 
   useKey('Enter', function () {
     if (document.activeElement === inputEl.current) {
@@ -12,23 +11,6 @@ export default function Search({ query, setQuery }) {
     inputEl.current.focus();
     setQuery('');
   });
-
-  // useEffect(() => {
-  //   function callback(e) {
-  //     if (document.activeElement === inputEl.current) {
-  //       return;
-  //     }
-
-  //     if (e.code === 'Enter') {
-  //       inputEl.current.focus();
-  //       setQuery('');
-  //     }
-  //   }
-  //   document.addEventListener('keydown', callback);
-  //   // console.log(inputEl.current);
-  //   return () => document.addEventListener('keydown', callback);
-  //   // inputEl.current.focus();
-  // }, [setQuery]);
 
   return (
     <input
@@ -41,62 +23,3 @@ export default function Search({ query, setQuery }) {
     />
   );
 }
-
-//====== 2
-// import { useEffect, useRef } from 'react';
-
-// export default function Search({ query, setQuery }) {
-//   useEffect(() => {
-//     const el = document.querySelector('.search');
-//     console.log(el);
-//     el.focus();
-//   }, []);
-
-//   return (
-//     <input
-//       className='search'
-//       type='text'
-//       placeholder='Search movies...'
-//       value={query}
-//       onChange={(e) => setQuery(e.target.value)}
-//     />
-//   );
-// }
-
-//====== 1
-// import { useEffect } from 'react';
-
-// export default function Search({ query, setQuery }) {
-//   // const el = document.querySelector('.search');
-//   // console.log(el);
-//   // el?.focus();
-
-//   useEffect(() => {
-//     const el = document.querySelector('.search');
-//     console.log(el);
-//     el.focus();
-//   }, []);
-
-//   return (
-//     <input
-//       className='search'
-//       type='text'
-//       placeholder='Search movies...'
-//       value={query}
-//       onChange={(e) => setQuery(e.target.value)}
-//     />
-//   );
-// }
-
-//====== orig
-// export default function Search({ query, setQuery }) {
-//   return (
-//     <input
-//       className='search'
-//       type='text'
-//       placeholder='Search movies...'
-//       value={query}
-//       onChange={(e) => setQuery(e.target.value)}
-//     />
-//   );
-// }
